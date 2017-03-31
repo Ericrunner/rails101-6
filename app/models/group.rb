66aa4b6c13a5ce27a5d2cea40cd2ed3645027f8e
liujia <ericrunner123456@yahoo.com>
2017-03-31 20:38:14 +0800

    implement join/quit group function

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,5 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   has_many :group_relations
-  has_many :members, through: :users, source: :group_relations
+  has_many :members, through: :group_relations, source: :user
 end
